5746eb7e2b6e5b24e3e6b25d59be75fdbe1f4ceb
dinosaurteaminfo <dinosaurteaminfo@gmail.com>
2026-04-07 11:23:38 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -11,13 +11,13 @@
 // ─────────────────────────────────────────────────────────────────────────────
 
 const firebaseConfig = {
-  apiKey:            "PASTE_YOUR_API_KEY_HERE",
-  authDomain:        "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  databaseURL:       "PASTE_YOUR_DATABASE_URL_HERE",
-  projectId:         "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket:     "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId:             "PASTE_YOUR_APP_ID_HERE",
+  apiKey:            "AIzaSyADMvNkir9mvt-TinV2g21DzB0HvLT0Rpo",
+  authDomain:        "dino-bucks.firebaseapp.com",
+  databaseURL:       "https://dino-bucks-default-rtdb.firebaseio.com",
+  projectId:         "dino-bucks",
+  storageBucket:     "dino-bucks.firebasestorage.app",
+  messagingSenderId: "904259174969",
+  appId:             "1:904259174969:web:2f939788eade3576d6c3e8",
 };
 
 export default firebaseConfig;
